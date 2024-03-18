dc32f2db5b53b37d0bc10bfbc212fd1acadb9114
Sandy Tam <111902534+satam2@users.noreply.github.com>
2024-03-18 15:23:25 -0700

    fixed LifeFactory to match new names

--- src/life/LifeFactory.java
+++ src/life/LifeFactory.java
@@ -3,19 +3,18 @@ package life;
 import calab.*;
 import mvc.*;
 
-
 public class LifeFactory extends GridFactory {
 
     @Override
     public Model makeModel() {
-        return new LifeGrid();
+        return new Society();
     }
 
     @Override
     public View makeView(Model m) {
-        if (!(m instanceof LifeGrid grid))
-            throw new RuntimeException("Model must instantiate LifeGrid");
-        return new LifeView(grid);
+        if (!(m instanceof Society grid))
+            throw new RuntimeException("Model must instantiate Society");
+        return new LifeGridView(grid);
     }
 
     @Override
@@ -42,16 +41,5 @@ public class LifeFactory extends GridFactory {
     @Override
     public String[] getEditCommands() {
         return new String[]{"Run1", "Run50", "Populate", "Clear"};
-    }
-
-    @Override
-    public Command makeEditCommand(Model model, String name, Object source) {
-        return switch (name) {
-            case "Clear" -> new ClearCommand(model);
-            case "Populate" -> new PopulateCommand(model);
-            case "Run1" -> new RunCommand(model, 1);
-            case "Run50" -> new RunCommand(model, 50);
-            default -> throw new IllegalArgumentException("Unexpected value: " + name);
-        };
     }
 }
