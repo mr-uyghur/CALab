aaa0a1e06bd971663b55677202b5155f1ccef1e6
rtk0c <mail.tianyig@gmail.com>
2024-03-18 15:13:59 -0700

    Fix imports and syntax in LifeCell

--- src/life/LifeCell.java
+++ src/life/LifeCell.java
@@ -1,23 +1,31 @@
-{
+package life;
+
+import calab.*;
+import java.awt.*;
+import java.util.*;
+import mvc.*;
+
+public class LifeCell extends Cell {
     private int status; // 0 is dead, 1 is alive
     private int ambience; // number of neighbors alive
     private Color color;
     private Set<Cell> neighbors;
 
-    public LifeCell(){
+    public LifeCell() {
         super.row = 20;
         super.col = 20;
         super.myGrid = new LifeGrid();
-        neighbors = myGrid.getNeighbors(this,1);
+        neighbors = myGrid.getNeighbors(this, 1);
         status = 0;
-        color = red;
+        color = Color.red;
         ambience = 0;
     }
+
     @Override
     public void observe() {
         int alive = 0;
-        for(Cell n: neighbors){
-            if(n.getStatus()==1){
+        for (Cell n : neighbors) {
+            if (n.getStatus() == 1) {
                 alive++;
             }
         }
@@ -48,25 +56,21 @@
     // checks ambience to determine status
     @Override
     public void nextState() {
-        if(ambience!=3&&ambience!=2){
+        if (ambience != 3 && ambience != 2) {
             status = 0;
-            color = red;
-        }
-        else{
+            color = Color.red;
+        } else {
             status = 1;
-            color = green;
+            color = Color.green;
         }
     }
 
     @Override
     public void reset(boolean randomly) {
-        if(randomly){ // set status to either dead or alive
-            if(rng.nextInt(2)==1)
-                status = 1;
-            else
-                status = 0;
-        }
-        else{ // set status to dead
+        if (randomly) { // set status to either dead or alive
+            if (Utilities.rng.nextInt(2) == 1) status = 1;
+            else status = 0;
+        } else { // set status to dead
             status = 0;
         }
     }
