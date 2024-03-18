4713c2d7de13c99a01272d836c23f5b969668d6c
Sandy Tam <111902534+satam2@users.noreply.github.com>
2024-03-18 15:28:18 -0700

    fixed object names in factory

--- src/life/LifeFactory.java
+++ src/life/LifeFactory.java
@@ -14,7 +14,7 @@ public class LifeFactory extends GridFactory {
     public View makeView(Model m) {
         if (!(m instanceof Society grid))
             throw new RuntimeException("Model must instantiate Society");
-        return new LifeGridView(grid);
+        return new GridView(grid);
     }
 
     @Override
@@ -36,10 +36,5 @@ public class LifeFactory extends GridFactory {
     @Override
     public String getAbout() {
         return "Game of Life v.1.0. Copyright 2024 by mvc group 2 (Tianyi, Sandy, Ali)";
-    }
-
-    @Override
-    public String[] getEditCommands() {
-        return new String[]{"Run1", "Run50", "Populate", "Clear"};
     }
 }
